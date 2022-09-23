374a2540b8145dd4e9401481dd2d62a3ba2521d1
Rajath Adiga T R <rajathatr19@gmail.com>
2022-09-23 19:05:03 +0530

    Doubly Linked List code added

--- src/com/rajath/Main.java
+++ src/com/rajath/Main.java
@@ -1,5 +1,6 @@
 package com.rajath;
 
+import com.rajath.data_structures.DoublyLinkedList;
 import com.rajath.data_structures.DynamicArray;
 
 public class Main {
@@ -8,8 +9,17 @@ public class Main {
         DynamicArray<Integer> dynamicArray = new DynamicArray();
         dynamicArray.add(10);
         dynamicArray.add(1);
+        dynamicArray.add(45);
         dynamicArray.add(100);
         dynamicArray.remove(10);
         System.out.println(dynamicArray.toString());
+        System.out.println(dynamicArray.size());
+
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList();
+        doublyLinkedList.add(10);
+        doublyLinkedList.add(20);
+        doublyLinkedList.add(30);
+        doublyLinkedList.remove(20);
+        System.out.println(doublyLinkedList.toString());
     }
 }
